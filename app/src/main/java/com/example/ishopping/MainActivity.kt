30f6d35a22d0a55df84e434ad1cdc5c63ef4e7bc
pengcon <vpdzjs@gmail.com>
2025-01-10 22:27:33 +0900

    refactor: 프래그먼트 임시 구별값 string 생성,setOnApplyWindowInsetsListener 주석 처리 (#2)

--- app/src/main/java/com/example/ishopping/MainActivity.kt
+++ app/src/main/java/com/example/ishopping/MainActivity.kt
@@ -19,11 +19,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
         setBottomNavigation()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 
     private fun setBottomNavigation() {
