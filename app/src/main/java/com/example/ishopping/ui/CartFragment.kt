a1fd50c7cbb8309ce0039ae0b8b631fa5b88868e
pengcon <vpdzjs@gmail.com>
2025-01-12 13:26:23 +0900

    refactor: ui 요소 패키지 이동 (#1)

--- app/src/main/java/com/example/ishopping/ui/CartFragment.kt
+++ app/src/main/java/com/example/ishopping/ui/CartFragment.kt
@@ -1,6 +1,7 @@
-package com.example.ishopping
+package com.example.ishopping.ui
 
 import androidx.fragment.app.Fragment
+import com.example.ishopping.R
 
 class CartFragment: Fragment(R.layout.fragment_cart) {
 }
--- app/src/main/java/com/example/ishopping/ui/LikeFragment.kt
+++ app/src/main/java/com/example/ishopping/ui/LikeFragment.kt
@@ -1,6 +1,7 @@
-package com.example.ishopping
+package com.example.ishopping.ui
 
 import androidx.fragment.app.Fragment
+import com.example.ishopping.R
 
 class LikeFragment : Fragment(R.layout.fragment_like) {
 }
