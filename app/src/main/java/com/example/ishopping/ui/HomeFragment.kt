eaefed2268f9051545da1816de3b603d0f3721e1
pengcon <vpdzjs@gmail.com>
2025-01-13 11:28:44 +0900

    feat: 해상도 별 이미지 가져오기 (#1)

--- app/src/main/java/com/example/ishopping/ui/HomeFragment.kt
+++ app/src/main/java/com/example/ishopping/ui/HomeFragment.kt
@@ -1,7 +1,33 @@
 package com.example.ishopping.ui
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.ishopping.R
+import com.example.ishopping.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
