4651c0b181f374b4e7627dc6b466d708b8208334
pengcon <vpdzjs@gmail.com>
2025-01-13 12:28:07 +0900

    feat: 검색화면에서 활용하는 검색화면 커스텀 뷰 구성 (#1)

---
+++ app/src/main/java/com/example/ishopping/ui/SearchLayout.kt
@@ -0,0 +1,13 @@
+package com.example.ishopping.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.ishopping.databinding.ViewSearchBinding
+
+class SearchLayout(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
+
+    private val binding: ViewSearchBinding =
+        ViewSearchBinding.inflate(LayoutInflater.from(context), this, true)
+}
