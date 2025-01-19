03a2cd06112f60efcc3f8f567e8ce871d32b9a33
pengcon <vpdzjs@gmail.com>
2025-01-19 18:01:32 +0900

    chore: retrofit,okhttp,gson,glide 추가 (#5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     kotlin("kapt")
     alias(libs.plugins.hilt)
 }
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
 
 android {
     namespace = "com.example.ishopping"
@@ -17,6 +22,13 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "NAVER_CLIENT_ID", properties.getProperty("naver.client.id"))
+        buildConfigField(
+            "String",
+            "NAVER_CLIENT_SECRET",
+            properties.getProperty("naver.client.secret")
+        )
     }
 
     buildTypes {
@@ -36,6 +48,7 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
+        buildConfig = true
         viewBinding = true
     }
 }
@@ -52,9 +65,22 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    //Gson
+    implementation(libs.gson)
+
+    //Glide
+    implementation(libs.glide)
+
     //hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
+
+    //networking
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson.converter)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
 }
 
 kapt {
