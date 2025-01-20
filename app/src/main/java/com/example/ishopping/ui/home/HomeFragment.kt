c30e5c0ebe6ca8a19aef2329b9d8e50de8340db7
pengcon <vpdzjs@gmail.com>
2025-01-20 18:38:38 +0900

    refactor: viewLifecycleOwner 적용 (#7)

--- app/src/main/java/com/example/ishopping/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/ishopping/ui/home/HomeFragment.kt
@@ -28,6 +28,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
         setLayout()
     }
 
