b4ec6565fc15171ffa0eedaaabca509115c5a9a9
pengcon <vpdzjs@gmail.com>
2025-01-20 15:01:40 +0900

    feat: Mainactivity,HomeRepository에 hilt 어노테이션 추가 (#7)

--- app/src/main/java/com/example/ishopping/data/source/HomeRepository.kt
+++ app/src/main/java/com/example/ishopping/data/source/HomeRepository.kt
@@ -2,8 +2,9 @@ package com.example.ishopping.data.source
 
 import com.example.ishopping.data.model.ShoppingItem
 import com.example.ishopping.data.source.remote.ShoppingService
+import jakarta.inject.Inject
 
-class HomeRepository(private val shoppingService: ShoppingService) {
+class HomeRepository @Inject constructor(private val shoppingService: ShoppingService) {
     suspend fun getShoppingItems(query: String): List<ShoppingItem> {
         return shoppingService.getShoppingItems(query).items
     }
