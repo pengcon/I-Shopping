3a877ef53e235e887009ee7928c339764c2d567e
pengcon <vpdzjs@gmail.com>
2025-01-26 11:37:26 +0900

    feat: searchFragment 생성 (#9)

--- app/src/main/java/com/example/ishopping/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/ishopping/ui/home/HomeFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.ishopping.R
 import com.example.ishopping.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModels<HomeShoppingItemViewmodel>()
@@ -43,6 +43,9 @@ class HomeFragment : Fragment() {
         viewModel.loadShoppingItems(getString(R.string.label_bag))
         viewModel.items.observe(viewLifecycleOwner) { shoppingItems ->
             adapter.addItems(shoppingItems)
+        }
+        binding.searchHomeLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_home_to_searchFragment)
         }
     }
 }
