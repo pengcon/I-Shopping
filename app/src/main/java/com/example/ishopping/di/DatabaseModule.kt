7e10932520b97c0981bf0b1a551716d7b9f7f94e
pengcon <vpdzjs@gmail.com>
2025-02-05 19:52:54 +0900

    feat: DatabaseModule 추가 (#11)

---
+++ app/src/main/java/com/example/ishopping/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.example.ishopping.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.ishopping.data.source.local.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context, AppDatabase::class.java,
+            "ishopping-db"
+        ).build()
+    }
+    
+    @Provides
+    fun provideBookmarkItemDao(appDatabase: AppDatabase) = appDatabase.bookmarkItemDao()
+}
