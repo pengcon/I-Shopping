2f07de33996b0bfab269a646f5e109dcd1535ca6
pengcon <vpdzjs@gmail.com>
2025-02-06 14:40:06 +0900

    feat: SearchShoppingItemAdapter에 BookmarkClickListener 적용 (#11)

--- app/src/main/java/com/example/ishopping/ui/search/SearchShoppingItemAdapter.kt
+++ app/src/main/java/com/example/ishopping/ui/search/SearchShoppingItemAdapter.kt
@@ -8,8 +8,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.ishopping.data.model.ItemUiModel
 import com.example.ishopping.databinding.ItemSearchShoppingItemBinding
 import com.example.ishopping.ui.search.SearchShoppingItemAdapter.SearchShoppingItemViewHolder
+import com.example.ishopping.util.BookmarkClickListener
 
-class SearchShoppingItemAdapter(diffCallback: DiffUtil.ItemCallback<ItemUiModel>) :
+class SearchShoppingItemAdapter(
+    diffCallback: DiffUtil.ItemCallback<ItemUiModel>,
+    private val listener: BookmarkClickListener
+) :
     PagingDataAdapter<ItemUiModel, SearchShoppingItemViewHolder>(diffCallback) {
 
 
@@ -17,7 +21,7 @@ class SearchShoppingItemAdapter(diffCallback: DiffUtil.ItemCallback<ItemUiModel>
         parent: ViewGroup,
         viewType: Int
     ): SearchShoppingItemViewHolder {
-        return SearchShoppingItemViewHolder.from(parent)
+        return SearchShoppingItemViewHolder.from(parent, listener)
     }
 
     override fun onBindViewHolder(
@@ -32,21 +36,27 @@ class SearchShoppingItemAdapter(diffCallback: DiffUtil.ItemCallback<ItemUiModel>
         }
     }
 
-    class SearchShoppingItemViewHolder(private val binding: ItemSearchShoppingItemBinding) :
+    class SearchShoppingItemViewHolder(
+        private val binding: ItemSearchShoppingItemBinding,
+        private val listener: BookmarkClickListener
+    ) :
         RecyclerView.ViewHolder(binding.root) {
-
         fun bind(itemUiModel: ItemUiModel) {
             binding.itemUiModel = itemUiModel
+            binding.listener = listener
         }
 
         companion object {
-            fun from(parent: ViewGroup): SearchShoppingItemViewHolder {
+            fun from(
+                parent: ViewGroup,
+                listener: BookmarkClickListener
+            ): SearchShoppingItemViewHolder {
                 val binding = ItemSearchShoppingItemBinding.inflate(
                     LayoutInflater.from(parent.context),
                     parent,
                     false
                 )
-                return SearchShoppingItemViewHolder(binding)
+                return SearchShoppingItemViewHolder(binding, listener)
             }
         }
     }
