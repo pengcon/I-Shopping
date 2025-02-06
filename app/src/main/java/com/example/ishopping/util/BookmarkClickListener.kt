8e7d4b55e9c42871a4dce5f8dee564601e557f07
pengcon <vpdzjs@gmail.com>
2025-02-06 14:39:11 +0900

    feat: BookmarkClickListener 적용 (#11)

---
+++ app/src/main/java/com/example/ishopping/util/BookmarkClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.ishopping.util
+
+import com.example.ishopping.data.model.ShoppingItem
+
+interface BookmarkClickListener {
+    fun onBookmarkButtonClick(shoppingItem: ShoppingItem, isBookmarked: Boolean)
+}
