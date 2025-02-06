d7c64ff250a8b8ab35ae71c0fbbc10684d159660
pengcon <vpdzjs@gmail.com>
2025-02-06 21:00:07 +0900

    feat: BookmarkViewmodel 구성 (#11)

---
+++ app/src/main/java/com/example/ishopping/ui/bookmark/BookmarkViewmodel.kt
@@ -0,0 +1,34 @@
+package com.example.ishopping.ui.bookmark
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ishopping.data.model.ShoppingItem
+import com.example.ishopping.data.source.BookmarkRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BookmarkViewmodel @Inject constructor(private val bookmarkRepository: BookmarkRepository) :
+    ViewModel() {
+
+    private val _bookmarkedItems = MutableStateFlow<List<ShoppingItem>>(emptyList())
+    val bookmarkedItems = _bookmarkedItems.asStateFlow()
+
+    fun loadBookmarkedItems() {
+        viewModelScope.launch {
+            bookmarkRepository.getAllBookmarkItems().collect { items ->
+                _bookmarkedItems.value = items
+            }
+        }
+    }
+
+    fun bookmarkButtonClick(shoppingItem: ShoppingItem) {
+        viewModelScope.launch(Dispatchers.IO) {
+            bookmarkRepository.deleteBookmarkItem(shoppingItem)
+        }
+    }
+}
