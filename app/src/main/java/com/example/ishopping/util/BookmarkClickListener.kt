ce82cd3f567815a77885f135fa849224ffdfb7a6
pengcon <vpdzjs@gmail.com>
2025-02-06 17:00:01 +0900

    refactor: BookmarkClickListener 파라미터 변경 (#11)

--- app/src/main/java/com/example/ishopping/util/BookmarkClickListener.kt
+++ app/src/main/java/com/example/ishopping/util/BookmarkClickListener.kt
@@ -3,5 +3,5 @@ package com.example.ishopping.util
 import com.example.ishopping.data.model.ShoppingItem
 
 interface BookmarkClickListener {
-    fun onBookmarkButtonClick(shoppingItem: ShoppingItem, isBookmarked: Boolean)
+    fun onBookmarkButtonClick(shoppingItem: ShoppingItem)
 }
