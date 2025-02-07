c087453157df89844a64bebd3e0c47cf55d9da88
pengcon <vpdzjs@gmail.com>
2025-02-07 23:18:23 +0900

    refactor: HomeShoppingItemAdapter ListAdapter로 변경 (#11)

--- app/src/main/java/com/example/ishopping/ui/home/HomeShoppingItemAdapter.kt
+++ app/src/main/java/com/example/ishopping/ui/home/HomeShoppingItemAdapter.kt
@@ -2,57 +2,55 @@ package com.example.ishopping.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ishopping.data.model.ShoppingItem
 import com.example.ishopping.databinding.ItemSearchShoppingItemBinding
 import com.example.ishopping.util.BookmarkClickListener
 
-class HomeShoppingItemAdapter(private val listener: BookmarkClickListener) :
-    RecyclerView.Adapter<HomeShoppingItemAdapter.ShoppingItemViewHolder>() {
-
-    private val homeItems = mutableListOf<ShoppingItem>()
-
+class HomeShoppingItemAdapter(
+    diffCallback: DiffUtil.ItemCallback<ShoppingItem>,
+    private val listener: BookmarkClickListener
+) : ListAdapter<ShoppingItem, HomeShoppingItemViewHolder>(diffCallback) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ShoppingItemViewHolder {
-        return ShoppingItemViewHolder.from(parent)
+    ): HomeShoppingItemViewHolder {
+        return HomeShoppingItemViewHolder.from(parent, listener)
     }
 
-    override fun getItemCount(): Int {
-        return homeItems.size
-    }
-
-    override fun onBindViewHolder(
-        holder: ShoppingItemViewHolder,
-        position: Int
-    ) {
-        holder.bind(homeItems[position], listener)
-    }
-
-    fun addItems(items: List<ShoppingItem>) {
-        val startPosition = items.size
-        homeItems.addAll(items)
-        notifyItemRangeInserted(startPosition, items.size)
-    }
-
-    class ShoppingItemViewHolder(private val binding: ItemSearchShoppingItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(shoppingItem: ShoppingItem, listener: BookmarkClickListener) {
-            binding.shoppingItem = shoppingItem
-            binding.listener = listener
-        }
-
-        companion object {
-            fun from(parent: ViewGroup): ShoppingItemViewHolder {
-                val binding = ItemSearchShoppingItemBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-                return ShoppingItemViewHolder(binding)
-            }
+    override fun onBindViewHolder(holder: HomeShoppingItemViewHolder, position: Int) {
+        val shoppingItem = getItem(position)
+        shoppingItem?.let { item ->
+            holder.bind(item)
+        } ?: run {
+            holder.bind(ShoppingItem.placeholder())
         }
     }
 }
+
+class HomeShoppingItemViewHolder(
+    private val binding: ItemSearchShoppingItemBinding,
+    private val listener: BookmarkClickListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(shoppingItem: ShoppingItem) {
+        binding.shoppingItem = shoppingItem
+        binding.listener = listener
+    }
+
+    companion object {
+        fun from(
+            parent: ViewGroup,
+            listener: BookmarkClickListener
+        ): HomeShoppingItemViewHolder {
+            val binding = ItemSearchShoppingItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+            return HomeShoppingItemViewHolder(binding, listener)
+        }
+    }
+}
+
