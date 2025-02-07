3cb9a0a20a011f947039edff241524a9d647bff1
pengcon <vpdzjs@gmail.com>
2025-02-07 22:22:49 +0900

    refactor: 람다 사용을 위한 fun interface로 변경 (#11)

--- app/src/main/java/com/example/ishopping/util/BookmarkClickListener.kt
+++ app/src/main/java/com/example/ishopping/util/BookmarkClickListener.kt
@@ -2,6 +2,6 @@ package com.example.ishopping.util
 
 import com.example.ishopping.data.model.ShoppingItem
 
-interface BookmarkClickListener {
+fun interface BookmarkClickListener {
     fun onBookmarkButtonClick(shoppingItem: ShoppingItem)
 }
