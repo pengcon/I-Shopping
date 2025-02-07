80d8e16acbe6c49e85e8a33e9cedea5b9303afcf
pengcon <vpdzjs@gmail.com>
2025-02-07 22:34:56 +0900

    feat: insertBookmarkItem, deleteBookmarkItem 추가,getShoppingItems에 Item.toShoppingItem() 적용 (#11)

--- app/src/main/java/com/example/ishopping/data/source/HomeRepository.kt
+++ app/src/main/java/com/example/ishopping/data/source/HomeRepository.kt
@@ -1,12 +1,27 @@
 package com.example.ishopping.data.source
 
 import com.example.ishopping.data.model.Item
+import com.example.ishopping.data.model.ShoppingItem
+import com.example.ishopping.data.model.toShoppingItem
+import com.example.ishopping.data.source.local.ShoppingItemDao
 import com.example.ishopping.data.source.remote.ShoppingService
 import javax.inject.Inject
 
-class HomeRepository @Inject constructor(private val shoppingService: ShoppingService) {
-    suspend fun getShoppingItems(query: String): List<Item> {
-        return shoppingService.getShoppingItems(query).items
+class HomeRepository @Inject constructor(
+    private val shoppingService: ShoppingService,
+    private val dao: ShoppingItemDao
+) {
+    suspend fun getShoppingItems(query: String): List<ShoppingItem> {
+        return shoppingService.getShoppingItems(query).items.map {
+            it.toShoppingItem()
+        }
     }
 
+    fun insertBookmarkItem(item: ShoppingItem) {
+        dao.insert(item)
+    }
+
+    fun deleteBookmarkItem(item: ShoppingItem) {
+        dao.delete(item)
+    }
 }
