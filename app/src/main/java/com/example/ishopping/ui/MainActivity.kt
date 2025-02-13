da34b6f3333c98af7428182a90f8b48be61ae8c8
pengcon <vpdzjs@gmail.com>
2025-02-13 18:39:08 +0900

    feat: mainActivity에 observeMode 추가 (#17)

--- app/src/main/java/com/example/ishopping/ui/MainActivity.kt
+++ app/src/main/java/com/example/ishopping/ui/MainActivity.kt
@@ -2,29 +2,47 @@ package com.example.ishopping.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.ishopping.R
 import com.example.ishopping.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewmodel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+        observeMode()
         setBottomNavigation()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
+        }
+    }
+
+    private fun observeMode() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.mode.collect { mode ->
+                    AppCompatDelegate.setDefaultNightMode(mode)
+                }
+            }
         }
     }
 
@@ -38,6 +56,5 @@ class MainActivity : AppCompatActivity() {
             setupWithNavController(navController)
         }
     }
-
 
 }
